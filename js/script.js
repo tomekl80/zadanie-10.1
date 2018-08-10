a598f33ac6eb07272c4092221e77944fc4b085eb
Tomasz Lis <tomasz.lis@o2.pl>
2018-08-10 16:40:50 +0200

    Change content files
    *Add CSS styles for button, carousel and container section
    *Add javascript code for "hash" option
    *Add button select first cell
    *Add  javascript code for scroll events
    *Remove pointer under carousel

--- js/script.js
+++ js/script.js
@@ -7,5 +7,29 @@ var flkty = new Flickity(elem, {
 	cellAlign: 'left',
 	contain: true,
 	freeScroll: true,
-	imageLoaded: true
+	imageLoaded: true,
+	hash: true,
 });
+
+// Stage 11 of excercise 10.1
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelector('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select(0);
+});
+
+// Stage 12 of excercise 10.1
+var progressBar = document.querySelector('.progress-bar');
+
+	flkty.on('scroll', function (progress) {
+		// body...
+		progress = Math.max(0, Math.min(1, progress));
+		progressBar.style.width = progress * 100 + '%';
+	});
